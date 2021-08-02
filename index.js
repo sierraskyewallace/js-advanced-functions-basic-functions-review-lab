deb28e3561c7fcb882a73dd459f6ca50feb58f33
sierraskyewallace <sskwallace@gmail.com>
2021-08-02 11:20:46 -0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+
+
+function saturdayFun(activity="roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+let mondayWork = function(activity="go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(flair="*") {
+  return function(adj="special") {
+    return `You are ${flair }${adj}${flair}!`;
+  }
+}
+
+const Calculator = {
+  add: function(a, b) {
+    return a+b;
+  },
+  subtract: function(a, b) {
+    return a-b;
+  },
+  multiply: function(a, b) {
+    return a*b;
+  },
+  divide: function(a, b) {
+    return a/b;
+  }
+}
+
+function actionApplyer(start, finish) {
+  for (let i = 0; i < finish.length; i++) {
+    start = finish[i](start);
+  }
+  return start;
+}
